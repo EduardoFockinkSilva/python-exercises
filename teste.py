bc35f185ffbe3980b7464071e5e946a8cdfed937
Eduardo Fockink Silva <eduardofockinksilva@gmail.com>
2020-04-27 14:13:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,3 +3,7 @@
 print('==================================')
 print('        this is just a test       ')
 print('==================================')
+
+num = int(input('insert a number: '))
+
+print((num * 5) % 3)
